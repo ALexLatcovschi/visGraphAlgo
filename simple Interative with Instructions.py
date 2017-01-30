8f3f43301b7f3341f057558c786da5e274866156
Mohit Kumar Jangid <mohit.wrangler@gmail.com>
2017-01-30 23:34:08 +0530

    1. Update code to show multiple Windows--one for graph--one for log text 2. Resize and Position Windows so that a learner can see both 3. Add window_destroyer function to delete each window 4. Add comments for window_destroyer function

--- simple Interative with Instructions.py
+++ simple Interative with Instructions.py
@@ -1,9 +1,9 @@
 from graph_tool.all import *
+from gi.repository import Gtk
+
 # We need some Gtk functions
-from gi.repository import Gtk, Gdk
 import sys
 import time
-
 
 # this is the input graph
 g = Graph()
@@ -56,7 +56,7 @@ pos = sfdp_layout(g)
 # append "vertex_" with "halo_color" and assign it equal to vHaloColor.
 # Its done.
 
-win = GraphWindow(g, pos,
+graph_window = GraphWindow(g, pos,
                   vertex_halo=False,
                   vertex_fill_color=vcolor,
                   vertex_text=g.vertex_index,
@@ -67,8 +67,10 @@ win = GraphWindow(g, pos,
                     geometry=(1000, 800))
 
 
+
+
 def button_press_event_function(widget, event):
-    global g, vcolor, win
+    global g, vcolor, graph_window
     # this when clicked with mouse left butten, returns |vertex| object to
     # |src|
     src = widget.picked
@@ -84,28 +86,79 @@ def button_press_event_function(widget, event):
     widget.regenerate_surface()
     widget.queue_draw()
 
-    # win.graph.connect("button-press-event", button_press_event_function)
+    # graph_window.graph.connect("button-press-event", button_press_event_function)
 
 
 def motion_event_function(widget, event):
     """This function is to handle any mouse movement over graph widget"""
-    global g, vcolor, win
+    
     """ for now, I am doing this to disable default mouse hover action,
     that is--all closest edges are highlighted and the focused vertex is
     highlighted with blue halo color   """
     widget.selected.fa = False
 
+def window_destroyer(theWindow, event):
+  """
+  theWindow: the window to be closed
+  event: delete-event object passed as signal
+
+  This function is called by graph and logger windows
+  using
+    graph_window.connect("delete_event", window_destroyer)
+    logger_window.connect("delete_event", window_destroyer)
+
+  This function destroyes the window which user
+  clicks to close x (cross) mark at top right corner.
+  It also keep tracks of count of closed window and 
+  if all windows are closed, it exits the GTK main program 
+   """
+  print 'In the destroyer function'
+  del theWindow
+  window_destroyer.destroy_count += 1
+  if window_destroyer.destroy_count == 2:
+    print 'Exiting Program'
+    Gtk.main_quit()     
+
+# function are really objects. window_destroyer.destroy_count here acts as static variable 
+window_destroyer.destroy_count = 0
+
+
 """ Bind the function above as a montion notify handler, see all events
 here https://graph-tool.skewed.de/static/doc/_modules/graph_tool/draw/gtk_draw.html"""
-win.graph.connect("button-press-event", button_press_event_function)
+graph_window.graph.connect("button-press-event", button_press_event_function)
 
 
-win.graph.connect("motion_notif_-event", motion_event_function)
+graph_window.graph.connect("motion-notify-event", motion_event_function)
 
 
 # We will give the user the ability to stop the program by closing the window.
-win.connect("delete_event", Gtk.main_quit)
+graph_window.connect("delete_event", window_destroyer)
 
-# Actually show the window, and start the main loop.
-win.show_all()
+logger_window=Gtk.Window()
+logger_window.connect("delete-event", window_destroyer)
+
+
+# let us set the logger_window as per logger window requirement
+# this function sets the size of window
+logger_window.resize(logger_window.get_screen().width() *.3,logger_window.get_screen().height())
+
+# setting window position
+# logger_window.set_pointer(0,0) --does not work
+logger_window.move(logger_window.get_screen().width() *.7,0)
+
+
+# let us set the graph_window as per logger window requirement
+# this function sets the size of window
+graph_window.resize(graph_window.get_screen().width() *.7,graph_window.get_screen().height())
+
+# setting window position
+# graph_window.set_pointer(0,0) --does not work
+graph_window.move(0,0)
+
+
+
+# Actually show the windows, and start the main loop.
+logger_window.show_all()
+graph_window.show_all()
 Gtk.main()
+
